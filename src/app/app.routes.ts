218b2ef22fe1949e0069393a998ea89f20c13c9f
nameless0422 <cjy042218@gmail.com>
2024-11-13 22:29:25 +0900

    app.routes 수정

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },        // 기본 경로에 홈 컴포넌트 연결
+  { path: 'login', component: LoginComponent }   // /login 경로에 로그인 컴포넌트 연결
+];
