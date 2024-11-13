5c3ae03c11627025af16e5637be14101e643db1d
nameless0422 <cjy042218@gmail.com>
2024-11-14 01:15:41 +0900

    그냥 이렇게 해야될듯

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiKey: process.env.TMDB_API_KEY // 프로덕션 환경의 API 키를 나중에 대체할 예정
+  apiKey: 'd4c0b3433a7bf98a9a57a56e9b2e1cbc' // 프로덕션 환경의 API 키를 나중에 대체할 예정
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiKey: process.env.TMDB_API_KEY // 개발 환경의 API 키를 설정 (필요할 경우)
+  apiKey: 'd4c0b3433a7bf98a9a57a56e9b2e1cbc' // 개발 환경의 API 키를 설정 (필요할 경우)
 };
