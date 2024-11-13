4858eb2823c25163e527800c38ee2455cce17b5b
nameless0422 <cjy042218@gmail.com>
2024-11-14 01:19:51 +0900

    explore 라우팅

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { WishlistComponent } from './pages/wishlist/wishlist.component';
+import { ExploreComponent } from './pages/explore/explore.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'explore', component: ExploreComponent },
   { path: 'wishlist', component: WishlistComponent }
 ];
