327d641affce3c8b7bacf0b4d4f3e6e5bfd3d561
nameless0422 <cjy042218@gmail.com>
2024-11-14 15:08:02 +0900

    movie.service.ts 수정

--- src/app/util/movie/movie.service.ts
+++ src/app/util/movie/movie.service.ts
@@ -8,25 +8,28 @@ import { environment } from '../../../environments/environment';
 export class MovieService {
   private apiKey = environment.apiKey;  // 발급받은 API 키로 변경
 
+  // 인기 영화 가져오기
   async getPopularMovies(page: number = 1): Promise<any> {
     const url = getURL4PopularMovies(this.apiKey, page);
     return await fetchMovies(url);
   }
 
+  // 최신 개봉 영화 가져오기
   async getReleaseMovies(page: number = 1): Promise<any> {
     const url = getURL4ReleaseMovies(this.apiKey, page);
     return await fetchMovies(url);
   }
 
-  async getActionMovies(page: number = 1): Promise<any> {
-    const actionGenreId = '28';  // 액션 장르 ID (TMDb 기준)
-    const url = getURL4GenreMovies(this.apiKey, actionGenreId, page);
-    return await fetchMovies(url);
-  }
-
+  // 영화 검색 기능
   async searchMovies(query: string): Promise<any> {
     const url = getURL4SearchMovies(this.apiKey, query);
     return await fetchMovies(url); // 검색 영화 요청을 URL.ts에서 처리
   }
-  
+
+  // 추가적인 장르 기반 영화 가져오기
+  async getMoviesByGenre(genreId: string, page: number = 1): Promise<any> {
+    const url = getURL4GenreMovies(this.apiKey, genreId, page);
+    return await fetchMovies(url);
+  }
 }
+
