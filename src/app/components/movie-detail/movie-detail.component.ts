27f2416f233f4fd1d46012154fbd1a3caf408f90
nameless0422 <cjy042218@gmail.com>
2024-11-14 23:02:24 +0900

    .

--- src/app/components/movie-detail/movie-detail.component.ts
+++ src/app/components/movie-detail/movie-detail.component.ts
@@ -1,10 +1,5 @@
-import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { MovieService } from '../../util/movie/movie.service';
-
-interface Video {
-  key: string;
-  site: string;
-}
 
 interface Genre {
   id: number;
@@ -17,34 +12,34 @@ interface Genre {
   styleUrls: ['./movie-detail.component.css'],
   standalone: true
 })
-export class MovieDetailComponent implements OnInit {
-  @Input() movie: any;
+export class MovieDetailComponent implements OnInit, OnChanges {
   @Input() movieId!: number;
   @Output() close = new EventEmitter<void>();  // 모달 닫기 이벤트 추가
+  movieDetails: any = null;
   movieImage: string = '';
-  movieDetails: any;
-  movieVideos: any[] = [];
   movieGenres: Genre[] = [];
+  movieVideos: any[] = [];
 
   constructor(private movieService: MovieService) {}
 
   ngOnInit(): void {
-    if (this.movie) {
-      // backdrop_path가 존재하지 않을 경우 poster_path 사용
-      const imagePath = this.movie.backdrop_path || this.movie.poster_path;
-      if (imagePath) {
-        this.movieImage = `https://image.tmdb.org/t/p/original${imagePath}`;
-      }
-      this.loadMovieVideos(this.movie.id);
-      this.loadMovieDetails(this.movie.id);
+    if (this.movieId) {
+      this.loadMovieDetails(this.movieId);
     }
   }
-  
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['movieId'] && changes['movieId'].currentValue) {
+      this.loadMovieDetails(changes['movieId'].currentValue);
+    }
+  }
+
   async loadMovieDetails(movieId: number): Promise<void> {
     try {
       this.movieDetails = await this.movieService.getMovieDetail(movieId);
-      this.movieImage = `https://image.tmdb.org/t/p/original${this.movieDetails.backdrop_path}`;
-      this.movieGenres = this.movieDetails.genres; // 장르 데이터 저장
+      this.movieImage = `https://image.tmdb.org/t/p/original${this.movieDetails.backdrop_path || this.movieDetails.poster_path}`;
+      this.movieGenres = this.movieDetails.genres || []; // 장르 데이터 저장
+      this.loadMovieVideos(movieId);
     } catch (error) {
       console.error('Error loading movie details:', error);
     }
@@ -53,7 +48,7 @@ export class MovieDetailComponent implements OnInit {
   async loadMovieVideos(movieId: number): Promise<void> {
     try {
       const videos = await this.movieService.getMovieVideos(movieId);
-      this.movieVideos = videos.filter((video:Video) => video.site === 'YouTube');
+      this.movieVideos = videos.filter((video: any) => video.site === 'YouTube');
     } catch (error) {
       console.error('Error loading movie videos:', error);
     }
@@ -63,4 +58,3 @@ export class MovieDetailComponent implements OnInit {
     this.close.emit();  // 부모 컴포넌트로 닫기 이벤트 전달
   }
 }
-
