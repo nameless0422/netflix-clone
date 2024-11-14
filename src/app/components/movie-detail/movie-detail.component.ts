4491292eb8c42631d1c9a2a7e7aacca7e5f6656d
nameless0422 <cjy042218@gmail.com>
2024-11-14 21:24:13 +0900

    모달 테스트

--- src/app/components/movie-detail/movie-detail.component.ts
+++ src/app/components/movie-detail/movie-detail.component.ts
@@ -1,16 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
-  standalone: true,
-  styleUrls: ['./movie-detail.component.css']
+  styleUrls: ['./movie-detail.component.css'],
+  standalone: true
 })
 export class MovieDetailComponent {
   @Input() movie: any;
+  @Output() close = new EventEmitter<void>();
 
   closeModal() {
-    // 모달 닫기 로직
     this.movie = null;
+    this.close.emit();  // 부모 컴포넌트에 모달 닫기 이벤트 전송
   }
 }
+
