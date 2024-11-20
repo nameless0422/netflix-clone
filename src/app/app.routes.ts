24d26a80abffeb45010f81b7db5f08a897d9c400
nameless0422 <cjy042218@gmail.com>
2024-11-21 01:56:30 +0900

    new 페이지 생성

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { NewComponent } from './pages/new/new.component';
 import { WishlistComponent } from './pages/wishlist/wishlist.component';
 import { ExploreComponent } from './pages/explore/explore.component';
 
@@ -10,5 +11,6 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'explore', component: ExploreComponent },
+  { path: 'new', component: NewComponent },
   { path: 'wishlist', component: WishlistComponent }
 ];
