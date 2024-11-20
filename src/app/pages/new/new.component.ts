bc2f593da7149e10177f4c8fb550c9077a1f2e80
nameless0422 <cjy042218@gmail.com>
2024-11-21 02:04:53 +0900

    new page 구현

--- src/app/pages/new/new.component.ts
+++ src/app/pages/new/new.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MovieService } from '../../util/movie/movie.service';
 
 @Component({
   selector: 'app-new',
-  standalone: true,
-  imports: [],
   templateUrl: './new.component.html',
-  styleUrl: './new.component.css'
+  styleUrls: ['./new.component.css'],
+  standalone: true,
+  imports: [CommonModule],
 })
-export class NewComponent {
+export class NewComponent implements OnInit {
+  movies: any[] = [];
+  page: number = 1;
+  isGridView: boolean = true;
 
+  constructor(private movieService: MovieService) {}
+
+  ngOnInit(): void {
+    this.loadReleaseMovies(); // 초기 데이터 로드
+  }
+
+  async loadReleaseMovies(): Promise<void> {
+    try {
+      const response = await this.movieService.getReleaseMovies(this.page);
+      if (response && response.results) {
+        this.movies = [...this.movies, ...response.results]; // 기존 데이터에 새 데이터 추가
+      } else {
+        console.error('No results found');
+      }
+    } catch (error) {
+      console.error('Error loading release movies:', error);
+    }
+  }
+  
+
+  // 무한스크롤
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    if (
+      window.innerHeight + window.scrollY >=
+      document.body.scrollHeight - 50
+    ) {
+      this.page++;
+      this.loadReleaseMovies();
+    }
+  }
+
+  // 뷰 전환
+  toggleView(): void {
+    this.isGridView = !this.isGridView;
+  }
 }
