9fc8141fca8c75f00c500752150f93b399c741c0
nameless0422 <cjy042218@gmail.com>
2024-11-20 16:35:50 +0900

    .

--- src/app/pages/wishlist/wishlist.component.ts
+++ src/app/pages/wishlist/wishlist.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit ,ChangeDetectorRef  } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import WishlistManager from '../../util/movie/useWishlist';
 
@@ -12,6 +12,7 @@ import WishlistManager from '../../util/movie/useWishlist';
 export class WishlistComponent implements OnInit {
   wishlist: any[] = [];
   private wishlistManager = new WishlistManager();
+  hoveredMovieId: number | null = null; // 마우스 호버 상태 관리
 
   constructor(private cdr: ChangeDetectorRef) {}
 
@@ -23,14 +24,23 @@ export class WishlistComponent implements OnInit {
     this.wishlistManager.loadWishlist();
     this.wishlist = this.wishlistManager.getWishlist();
     console.log('Loaded Wishlist:', this.wishlist); // 디버깅용
-    this.cdr.detectChanges();
+    this.cdr.detectChanges(); // 상태 변경 강제 반영
   }
 
-  removeFromWishlist(movieId: number): void {
-    const movie = this.wishlist.find((item) => item.id === movieId);
-    if (movie) {
-      this.wishlistManager.toggleWishlist(movie);
-      this.loadWishlist();
-    }
+  isMovieInWishlist(movieId: number): boolean {
+    return this.wishlistManager.isInWishlist(movieId); // 위시리스트에 영화가 있는지 확인
+  }
+
+  toggleFavorite(movie: any): void {
+    this.wishlistManager.toggleWishlist(movie); // 좋아요 상태 토글
+    this.loadWishlist(); // 위시리스트 다시 로드
+  }
+
+  onMouseEnter(movieId: number): void {
+    this.hoveredMovieId = movieId; // 마우스가 영화 카드 위로 올라갔을 때
+  }
+
+  onMouseLeave(): void {
+    this.hoveredMovieId = null; // 마우스가 영화 카드에서 벗어났을 때
   }
 }
