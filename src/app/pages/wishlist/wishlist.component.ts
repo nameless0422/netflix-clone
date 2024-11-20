d5d81942add0345a167e8c57ed1403544643651f
nameless0422 <cjy042218@gmail.com>
2024-11-20 14:10:45 +0900

    .

--- src/app/pages/wishlist/wishlist.component.ts
+++ src/app/pages/wishlist/wishlist.component.ts
@@ -1,49 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import WishlistManager from '../../util/movie/useWishlist';
 
-interface Movie {
-  id: number;
-  title: string;
-  poster_path: string;
-}
-
 @Component({
   selector: 'app-wishlist',
   templateUrl: './wishlist.component.html',
-  styleUrls: ['./wishlist.component.css']
+  styleUrls: ['./wishlist.component.css'],
 })
 export class WishlistComponent implements OnInit {
-  wishlist: Movie[] = [];
-  isLoading: boolean = true;
-
-  constructor(private wishlistManager: WishlistManager) {}
+  wishlist: any[] = [];
+  private wishlistManager = new WishlistManager();
 
   ngOnInit(): void {
     this.loadWishlist();
   }
 
   loadWishlist(): void {
-    try {
-      this.isLoading = true;
-      this.wishlist = this.wishlistManager.getWishlist();
-      console.log('Loaded Wishlist:', this.wishlist);
-    } catch (error) {
-      console.error('Error loading wishlist:', error);
-      this.wishlist = [];
-    } finally {
-      this.isLoading = false;
-    }
+    this.wishlistManager.loadWishlist();
+    this.wishlist = this.wishlistManager.getWishlist();
+    console.log('Loaded Wishlist:', this.wishlist); // 디버깅용
   }
 
   removeFromWishlist(movieId: number): void {
-    try {
-      const movie = this.wishlist.find((item) => item.id === movieId);
-      if (movie) {
-        this.wishlistManager.toggleWishlist(movie);
-        this.loadWishlist();
-      }
-    } catch (error) {
-      console.error('Error removing movie from wishlist:', error);
+    const movie = this.wishlist.find((item) => item.id === movieId);
+    if (movie) {
+      this.wishlistManager.toggleWishlist(movie);
+      this.loadWishlist();
     }
   }
 }
