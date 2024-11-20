95753030bac257aa8b2f2344b51855f747397160
nameless0422 <cjy042218@gmail.com>
2024-11-20 13:58:09 +0900

    좀 돼라

--- src/app/pages/wishlist/wishlist.component.ts
+++ src/app/pages/wishlist/wishlist.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import WishlistManager from '../../util/movie/useWishlist';
 
 @Component({
   selector: 'app-wishlist',
   templateUrl: './wishlist.component.html',
-  styleUrls: ['./wishlist.component.css']
+  styleUrls: ['./wishlist.component.css'],
 })
 export class WishlistComponent implements OnInit {
   wishlist: any[] = [];
   private wishlistManager = new WishlistManager();
-
-  constructor(private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     this.loadWishlist();
@@ -19,8 +17,7 @@ export class WishlistComponent implements OnInit {
   loadWishlist(): void {
     this.wishlistManager.loadWishlist();
     this.wishlist = this.wishlistManager.getWishlist();
-    console.log('Wishlist Data:', this.wishlist); // 디버깅용
-    this.cdr.detectChanges(); // 강제로 Change Detection 수행
+    console.log('Loaded Wishlist:', this.wishlist); // 디버깅용
   }
 
   removeFromWishlist(movieId: number): void {
