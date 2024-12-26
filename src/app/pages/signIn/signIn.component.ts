1d35d24c119bc8a4d5b896a8a985dc3ddabdc220
nameless0422 <cjy042218@gmail.com>
2024-12-26 16:44:57 +0900

    .

--- src/app/pages/signIn/signIn.component.ts
+++ src/app/pages/signIn/signIn.component.ts
@@ -102,7 +102,7 @@ export class SignInComponent {
   // 카카오 로그인 처리
   onKakaoLogin() {
     Kakao.Auth.authorize({
-      redirectUri: 'https://nameless0422.github.io/netflix-clone/kakao-callback',
+      redirectUri: 'https://nameless0422.github.io/netflix-clone/signIn',
     });
     Kakao.Auth.login({
       success: (authObj: any) => {
