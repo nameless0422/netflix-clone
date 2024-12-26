8793dcce58ee4566d807733c084e7f0976d4fbc5
nameless0422 <cjy042218@gmail.com>
2024-12-26 13:06:08 +0900

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,5 @@
-import * as dotenv from 'dotenv';
-
-dotenv.config();
-
 export const environment = {
     production: true,
-    apiKey: process.env['TMDB_API_KEY'] || '',
-    kakaoApiKey: process.env['KAKAO_API_KEY'] || '',
+    apiKey: ['TMDB_API_KEY'] || '',
+    kakaoApiKey: ['KAKAO_API_KEY'] || '',
 };
